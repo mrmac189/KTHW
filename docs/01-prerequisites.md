4fe8219810946530e618af978f130a0af8b0df22
mrmac189 <33000a@gmail.com>
2026-03-21 17:31:49 +0100

    01-prerequisites.md redone with Gentoo and Terraform + Libvirt

--- docs/01-prerequisites.md
+++ docs/01-prerequisites.md
@@ -2,18 +2,74 @@
 
 In this lab you will review the machine requirements necessary to follow this tutorial.
 
-## Virtual or Physical Machines
+## Virtual Machines
 
-This tutorial requires four (4) virtual or physical ARM64 or AMD64 machines running Debian 12 (bookworm). The following table lists the four machines and their CPU, memory, and storage requirements.
+This tutorial requires four (4) virtual AMD64 machines running Gentoo. The following table lists the four machines and their CPU, memory, and storage requirements.
 
 | Name    | Description            | CPU | RAM   | Storage |
 |---------|------------------------|-----|-------|---------|
-| jumpbox | Administration host    | 1   | 512MB | 10GB    |
+| jumpbox | Administration host    | 1   | 2GB   | 20GB    |
 | server  | Kubernetes server      | 1   | 2GB   | 20GB    |
 | node-0  | Kubernetes worker node | 1   | 2GB   | 20GB    |
 | node-1  | Kubernetes worker node | 1   | 2GB   | 20GB    |
 
-How you provision the machines is up to you, the only requirement is that each machine meet the above system requirements including the machine specs and OS version. Once you have all four machines provisioned, verify the OS requirements by viewing the `/etc/os-release` file:
+## Host system
+My system is Gentoo host, running QEMU/libvirt. The following guides should be referenced to install the hypervisor platform:
+- https://wiki.gentoo.org/wiki/Libvirt
+- https://wiki.gentoo.org/wiki/Qemu
+- https://wiki.gentoo.org/wiki/Virt-manager
+
+Following Portage USE-Flags are used:
+
+/etc/portage/package.use/libvirt
+```bash
+app-emulation/libvirt pcap virt-network numa fuse macvtap vepa qemu libssh2 bash-completion libvirtd
+```
+
+/etc/portage/package.use/qemu
+```bash
+app-emulation/qemu usbredir spice wayland virtfs doc QEMU_SOFTMMU_TARGETS: x86_64 QEMU_USER_TARGETS: x86_64
+```
+
+/etc/portage/package.use/virt-manager 
+```bash
+app-emulation/virt-manager gui
+```
+
+If libvirt can not activate the vnet, following kernel parameters may be need to be added: 
+- NF_TABLES
+- NF_NAT
+- NF_CONNTRACK
+- NFT_NAT
+- NFT_MASQ
+- NFT_CT
+
+## Guest systems provisioning
+To make guest VMs setup less mouse-clicky, I am going to use Terraform to provision the 4 VMs, by using the https://github.com/dmacvicar/terraform-provider-libvirt module. 
+
+Gentoo has nowadays the cloud-init qcow2 images - https://www.gentoo.org/news/2025/02/20/gentoo-qcow2-images.html. We could use them direct to keep the guests installation simpler.
+
+Before using the Terraform, one may need to test the virtualisation platform and having all the modules in kernel installed. I set a test VM up with virt-manager, using "no root pw" qcow2 image.
+- QCOW2 image supports exclusively UEFI, that must be configured direct during the setup.
+
+```xml
+<os firmware="efi">
+   <type arch="x86_64" machine="pc-q35-10.2">hvm</type>
+   <firmware>
+     <feature enabled="no" name="enrolled-keys"/>
+     <feature enabled="no" name="secure-boot"/>
+   </firmware>
+   <loader readonly="yes" type="pflash" format="qcow2">/usr/share/edk2/OvmfX64/OVMF_CODE_4M.qcow2</loader>
+```
+
+After the correct functionality of VM is determined, the test VM can be deleted und it may be proceeded the approach with Terraform. 
+
+- libvirtd daemon is the only method supported by dmacvicar/terraform-provider-libvirt, new modular architecture seems to not be supported (I needed to disable virt*d services and enable libvirtd)
+
+
+
+
+Once all four machines are provisioned, verify the OS requirements by viewing the `/etc/os-release` file:
 
 ```bash
 cat /etc/os-release
@@ -22,12 +78,11 @@ cat /etc/os-release
 You should see something similar to the following output:
 
 ```text
-PRETTY_NAME="Debian GNU/Linux 12 (bookworm)"
-NAME="Debian GNU/Linux"
-VERSION_ID="12"
-VERSION="12 (bookworm)"
-VERSION_CODENAME=bookworm
-ID=debian
+NAME=Gentoo
+ID=gentoo
+PRETTY_NAME="Gentoo Linux"
+ANSI_COLOR="1;32"
+HOME_URL="https://www.gentoo.org/"
 ```
 
 Next: [setting-up-the-jumpbox](02-jumpbox.md)
---
+++ tf/main.tf
@@ -0,0 +1,296 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = ">= 0.9.4"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+variable "ssh_public_key" {
+  description = "Public SSH key for the cowboy user"
+  type        = string
+}
+
+variable "libvirt_pool" {
+  description = "Existing libvirt storage pool"
+  type        = string
+  default     = "default"
+}
+
+variable "libvirt_network" {
+  description = "Existing libvirt network"
+  type        = string
+  default     = "default"
+}
+
+variable "ovmf_code" {
+  description = "Path to OVMF_CODE on the host"
+  type        = string
+  default     = "/usr/share/edk2/OvmfX64/OVMF_CODE.fd"
+}
+
+variable "ovmf_vars" {
+  description = "Path to OVMF_VARS template on the host"
+  type        = string
+  default     = "/usr/share/edk2/OvmfX64/OVMF_VARS.fd"
+}
+
+variable "cowboy_password_hash" {
+  type      = string
+  sensitive = true
+}
+
+locals {
+  gib = 1024 * 1024 * 1024
+
+  gentoo_cloud_image = "https://distfiles.gentoo.org/releases/amd64/autobuilds/20260308T170100Z/di-amd64-cloudinit-20260308T170100Z.qcow2"
+
+  machines = {
+    jumpbox = {
+      vcpu      = 1
+      memory_mb = 2048
+      disk_size = 20 * local.gib
+    }
+    server = {
+      vcpu      = 1
+      memory_mb = 2048
+      disk_size = 20 * local.gib
+    }
+    node-0 = {
+      vcpu      = 1
+      memory_mb = 2048
+      disk_size = 20 * local.gib
+    }
+    node-1 = {
+      vcpu      = 1
+      memory_mb = 2048
+      disk_size = 20 * local.gib
+    }
+  }
+}
+
+resource "libvirt_volume" "gentoo_base" {
+  name = "di-amd64-cloudinit-20260308T170100Z.qcow2"
+  pool = var.libvirt_pool
+
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
+
+  create = {
+    content = {
+      url = local.gentoo_cloud_image
+    }
+  }
+}
+
+resource "libvirt_volume" "root_disk" {
+  for_each = local.machines
+
+  name     = "${each.key}.qcow2"
+  pool     = var.libvirt_pool
+  capacity = each.value.disk_size
+
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
+
+  backing_store = {
+    path = libvirt_volume.gentoo_base.path
+
+    format = {
+      type = "qcow2"
+    }
+  }
+}
+
+resource "libvirt_cloudinit_disk" "seed" {
+  for_each = local.machines
+
+  name = "${each.key}-seed"
+
+  user_data = <<-EOF
+    #cloud-config
+    hostname: ${each.key}
+    fqdn: ${each.key}.kthw.local
+    manage_etc_hosts: true
+    ssh_pwauth: false
+
+    users:
+      - name: cowboy
+        gecos: Cowboy User
+        groups: [wheel]
+        sudo: ALL=(ALL) NOPASSWD:ALL
+        shell: /bin/bash
+        ssh_authorized_keys:
+          - ${var.ssh_public_key}
+        hashed_passwd: ${var.cowboy_password_hash}
+
+    runcmd:
+      - [ sh, -lc, "systemctl enable --now sshd" ]
+  EOF
+
+  meta_data = <<-EOF
+    instance-id: ${each.key}
+    local-hostname: ${each.key}
+  EOF
+}
+
+resource "libvirt_volume" "seed_iso" {
+  for_each = local.machines
+
+  name = "${each.key}-seed.iso"
+  pool = var.libvirt_pool
+
+  create = {
+    content = {
+      url = libvirt_cloudinit_disk.seed[each.key].path
+    }
+  }
+}
+
+resource "libvirt_domain" "vm" {
+  for_each = local.machines
+
+  name        = each.key
+  memory      = each.value.memory_mb
+  memory_unit = "MiB"
+  vcpu        = each.value.vcpu
+  type        = "kvm"
+  autostart   = true
+  running     = true
+
+  features = {
+  	acpi = true
+	}
+
+  os = {
+    type         = "hvm"
+    type_arch    = "x86_64"
+    type_machine = "q35"
+
+    firmware        = "efi"
+    loader          = var.ovmf_code
+
+    nv_ram = {
+      nv_ram   = "/var/lib/libvirt/qemu/nvram/${each.key}.fd"
+      template = var.ovmf_vars
+    }
+
+    boot_devices = [
+        {
+          dev = "hd"
+        }
+      ]
+  }
+
+  devices = {
+    disks = [
+      {
+        source = {
+          volume = {
+            pool   = libvirt_volume.root_disk[each.key].pool
+            volume = libvirt_volume.root_disk[each.key].name
+          }
+        }
+
+        target = {
+          dev = "vda"
+          bus = "virtio"
+        }
+
+        driver = {
+          type = "qcow2"
+        }
+      },
+      {
+        device = "cdrom"
+
+        source = {
+          volume = {
+            pool   = libvirt_volume.seed_iso[each.key].pool
+            volume = libvirt_volume.seed_iso[each.key].name
+          }
+        }
+
+        target = {
+          dev = "sda"
+          bus = "sata"
+        }
+      }
+    ]
+
+    interfaces = [
+      {
+        model = {
+          type = "virtio"
+        }
+
+        source = {
+          network = {
+            network = var.libvirt_network
+          }
+        }
+
+	wait_for_ip = {
+  		timeout = 300
+  		source  = "lease"
+	}
+      }
+    ]
+
+    consoles = [
+      {
+        type        = "pty"
+        target_type = "serial"
+        target_port = "0"
+      }
+    ]
+
+    rngs = [
+      {
+        model = "virtio"
+        backend = {
+          random = "/dev/urandom"
+        }
+      }
+    ]
+
+    graphics = [
+      {
+        vnc = {
+          auto_port = true
+          listen    = "127.0.0.1"
+        }
+      }
+    ]
+    video = {
+      type = "virtio"
+    }
+  }
+}
+
+data "libvirt_domain_interface_addresses" "vm" {
+  for_each = local.machines
+
+  domain = libvirt_domain.vm[each.key].name
+  source = "lease"
+}
+
+output "vm_ips" {
+  value = {
+    for name, iface in data.libvirt_domain_interface_addresses.vm :
+    name => try(iface.interfaces[0].addrs[0].addr, "no-ip-yet")
+  }
+}
